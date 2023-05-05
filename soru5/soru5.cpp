37273385878428cffbe0dfc7f581895086f0f9ab
metekurt41 <126515854+metekurt41@users.noreply.github.com>
2023-05-05 18:47:43 +0300

    soru5

---
+++ soru5/soru5.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <stdio.h>
+#include <string.h>
+
+void removeDuplicates(char* str) {
+    int i = 0;
+    int j = 1;
+    int len = strlen(str);
+    while (j < len) {
+        if (str[i] == str[j]) {
+            j++;
+        }
+        else {
+            i++;
+            str[i] = str[j];
+            j++;
+        }
+    }
+    str[i + 1] = '\0';
+}
+
+int main() {
+    char str[] = "DBAABDAB";
+    removeDuplicates(str);
+    printf("%s", str);
+    return 0;
+}
+
